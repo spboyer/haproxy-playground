1b6301c53862e0ecca6578806516f815690ddf98
Fabien Dehopré <fabien@dehopre.com>
2016-05-24 11:46:31 +0200

    SAVEPOINT

--- web/src/app/shared/hero.service.ts
+++ web/src/app/shared/hero.service.ts
@@ -3,6 +3,7 @@ import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Hero } from './hero';
 import 'rxjs/add/operator/map';
+import { environment } from '../environment';
 
 @Injectable()
 export class HeroService {
@@ -10,13 +11,29 @@ export class HeroService {
   }
 
   getHeroes(): Observable<Hero[]> {
-    return this._http.get('/api/heroes').map(
+    let url: string;
+    if (environment.production) {
+      url = '/api/heroes';
+    }
+    else {
+      url = 'http://localhost:3000/heroes';
+    }
+
+    return this._http.get(url).map(
       response => <Hero[]>response.json()
     );
   }
 
   getHero(id: number): Observable<Hero> {
-    return this._http.get('/api/heroes/' + id).map(
+    let url: string;
+    if (environment.production) {
+      url = '/api/heroes/' + id;
+    }
+    else {
+      url = 'http://localhost:3000/heroes/' + id;
+    }
+
+    return this._http.get(url).map(
       response => <Hero>response.json()
     );
   }
--- web/src/app/toh.component.ts
+++ web/src/app/toh.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { Title } from '@angular/platform-browser';
+import { DashboardComponent } from './+dashboard';
+import { Routes, Router, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
+import { HeroService } from './shared';
 
 @Component({
   moduleId: module.id,
   selector: 'toh-app',
   templateUrl: 'toh.component.html',
-  styleUrls: ['toh.component.css']
+  styleUrls: ['toh.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, HTTP_PROVIDERS, HeroService]
 })
+@Routes([
+  {path: '/dashboard', component: DashboardComponent/*, useAsDefault: true*/}
+])
 export class TohAppComponent implements OnInit {
   title = 'Tour of Heroes';
 
-  constructor(private _title: Title) {
+  constructor(private _title: Title, private _router: Router) {
   }
 
   ngOnInit() {
     this._title.setTitle(this.title);
+    this._router.navigate(['/dashboard']);
   }
 }
